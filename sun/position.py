bf3d238ba9bea1fdf4f377b9b48b833c76bd5747
Jurgen Strydom <jurgen.strydom@gmail.com>
2023-03-14 12:31:16 +0200

    make it so that datetimes use timezones - not always assuming a local datetime

--- sun/position.py
+++ sun/position.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timedelta
-from math import sin, cos, asin, acos
 import numpy as np
+import pytz, tzlocal
 
 sind = lambda degrees: np.sin(np.deg2rad(degrees))
 cosd = lambda degrees: np.cos(np.deg2rad(degrees))
-asind = lambda ratio: np.rad2deg(asin(ratio))
-acosd = lambda ratio: np.rad2deg(acos(ratio))
+arcsind = lambda ratio: np.rad2deg(np.arcsin(ratio))
+arccosd = lambda ratio: np.rad2deg(np.arccos(ratio))
 
 class SunAtLatLon():
 
@@ -13,12 +13,24 @@ class SunAtLatLon():
         self.lat = lat
         self.lon = lon
 
+    @staticmethod
+    def __localize_tz(when):
+        if when.tzinfo is None or when.tzinfo.utcoffset(when) is None:
+            local_tz = tzlocal.get_localzone()
+            tz = pytz.timezone(local_tz._zone.key)
+            when = tz.localize(when)
+        return when, when.tzinfo.zone
+
     def at_datetime(self, when: datetime):
         # code implement from 
         # https://www.pveducation.org/pvcdrom/properties-of-sunlight/elevation-angle
-        
-        LSTM = 15*np.round((datetime.now()-datetime.utcnow()).seconds/60/60)
-        N_dt = (when-datetime(when.year,1,1,0,0))
+        # print(when, when.tzinfo, when.tzinfo.utcoffset(when))
+        when, tzinfo = self.__localize_tz(when)
+        tz = pytz.timezone(tzinfo)
+        tz_offset_hours = when.utcoffset().seconds/60/60
+        LSTM = 15*tz_offset_hours
+        start_of_year = tz.localize(datetime(when.year,1,1,0,0))
+        N_dt = (when-start_of_year)
         Nd = N_dt.days + 1
         B = (360/365)*(Nd-81)
         EoT = 9.87*sind(2*B)-7.53*cosd(B)-1.5*sind(B)
@@ -27,8 +39,8 @@ class SunAtLatLon():
         HRA = 15*(LST.hour + LST.minute/60 + LST.second/60/60 - 12)
         delta = 23.45*sind((360/365)*(Nd-81)) # declanation
         theta = self.lat
-        alpha = asind(sind(delta)*sind(theta) + cosd(delta)*cosd(theta)*cosd(HRA)) # elevation
-        az = acosd((sind(delta)*cosd(theta) - cosd(delta)*sind(theta)*cosd(HRA))/cosd(alpha)) # azimuth uncorrected
+        alpha = arcsind(sind(delta)*sind(theta) + cosd(delta)*cosd(theta)*cosd(HRA)) # elevation
+        az = arccosd((sind(delta)*cosd(theta) - cosd(delta)*sind(theta)*cosd(HRA))/cosd(alpha)) # azimuth uncorrected
         azimuth = az if HRA <0 else 360 - az
         elevation = alpha
 
--- sun_position_interface.py
+++ sun_position_interface.py
@@ -1,11 +1,23 @@
 from datetime import datetime
 from sun.position import SunAtLatLon
+import pytz
 
+def main():
+    date = datetime(2023,1,1,12,00)
+    print(date.tzinfo)
 
-date = datetime(2023,3,13,12,10)
-lat, lon = -25.8932, 28.2185
+    # tz = pytz.timezone('Africa/Johannesburg')
+    # date = tz.localize(date)
 
-sun_angles = SunAtLatLon(lat, lon)
-angles = sun_angles.at_datetime(date)
-print(angles)
+    # print(date.tzinfo, date.tzinfo.utcoffset(date))
 
+    # print(date.utcoffset().seconds/60/60)
+
+    lat, lon = 40, -20
+
+    sun_angles = SunAtLatLon(lat, lon)
+    angles = sun_angles.at_datetime(date)
+    print(angles)
+
+if __name__ == '__main__':
+    main()
